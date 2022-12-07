12a0c836764131aeabb3a9255a20bd8989333998
Willian Rodrigues <will@wcr.design>
2022-12-07 15:48:09 -0300

    Update index.js

--- src/Pages/Briefing/index.js
+++ src/Pages/Briefing/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './style.css';
-import { Widget } from '@typeform/embed-react'
+import { Widget } from '@typeform/embed-react';
+import { useSearchParams } from 'react-router-dom';
 
 function Briefing(props) {
+    let [searchParams, setSearchParams] = useSearchParams();
+
     document.title = 'Briefing | wcr.design';
     
+    console.log(searchParams.get('test'));
+
     return (
-        <Widget id="Y27iy3K9" className="briefing-form" />
+        <Widget id="Y27iy3K9" className="briefing-form" enableSandbox={searchParams.get('test') ? true : false}/>
     );
 }
 
