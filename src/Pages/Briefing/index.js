aef9ff74720a0684d47452cf70cc110e4752dcbe
Willian Rodrigues <will@wcr.design>
2022-12-07 15:22:14 -0300

    Changed logo and title

--- src/Pages/Briefing/index.js
+++ src/Pages/Briefing/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './style.css';
 import { Widget } from '@typeform/embed-react'
 
-function Briefing() {
+function Briefing(props) {
+    document.title = 'Briefing | wcr.design';
+    
     return (
         <Widget id="Y27iy3K9" className="briefing-form" />
     );
