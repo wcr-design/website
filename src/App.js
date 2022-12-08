d988c44b0f041550cc097e64e2949dfa4f052838
Willian Rodrigues <will@wcr.design>
2022-12-08 12:29:26 -0300

    Added GTM Tag and event in briefing form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import RoutesList from "./RoutesList";
+import TagManager from 'react-gtm-module'
+
+TagManager.initialize({
+  gtmId: 'GTM-T859MJM'
+})
 
 function App() {
   return (
--- src/Pages/Briefing/index.js
+++ src/Pages/Briefing/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import './style.css';
 import { Widget } from '@typeform/embed-react';
 import { useSearchParams } from 'react-router-dom';
+import TagManager from 'react-gtm-module';
 
 function Briefing(props) {
     let [searchParams, setSearchParams] = useSearchParams();
 
     document.title = 'Briefing | wcr.design';
-    
-    console.log(searchParams.get('test'));
+
+    function sendForm(){
+        TagManager.dataLayer({
+            dataLayer: {
+                event: "briefing_form_submit",
+            }
+        });
+    }
 
     return (
-        <Widget id="Y27iy3K9" className="briefing-form" enableSandbox={searchParams.get('test') ? true : false}/>
+        <Widget
+            id="Y27iy3K9"
+            className="briefing-form"
+            enableSandbox={searchParams.get('test') ? true : false}
+            onSubmit={sendForm}
+        />
     );
 }
 
